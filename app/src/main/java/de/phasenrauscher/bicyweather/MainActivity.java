51a8062b12b3c927d03c3b5d0d7883a4acb68b0c
phasenrauscher <phasenrauscher@gmail.com>
2024-08-11 20:21:20 +0200

    Initial commit

--- app/src/main/java/de/phasenrauscher/bicyweather/MainActivity.java
+++ app/src/main/java/de/phasenrauscher/bicyweather/MainActivity.java
@@ -364,13 +364,14 @@ public class MainActivity extends AppCompatActivity{
         }
         */
 
+        /* m.s. 11AUG24 commented
         // storage runtime permission
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
                 requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE);
             }
         }
-
+        */
 
         // if data base does not exits, create one with default values
         String dbname = "Userdata.db";
